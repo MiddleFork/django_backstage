04e412782b4d8d578f707d533bfde9f1dc7c5b68
= <walker@mfgis.com>
2014-07-02 14:27:16 -0700

    shortcuts

--- backstage/shortcuts.py
+++ backstage/shortcuts.py
@@ -1,3 +1,9 @@
+from backstage.act.act import Act
+from backstage.venue.venue import Venue
+
+from backstage.venue.venue_utils import  new_venue, venue_from_cwd
+from backstage.act.act_utils import  new_act
+
 
 
 
